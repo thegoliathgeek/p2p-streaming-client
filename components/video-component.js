5ebe3994ced4d5e3c713f53ee347a14a5ca25797
imdhanush <dhanushdazz@gmail.com>
2021-06-04 13:47:24 +0530

    feat: video record option

--- components/video-component.js
+++ components/video-component.js
@@ -1,20 +1,53 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const VideoComponent = ({ callback, reference }) => {
   const videoRef = useRef(null);
+  const [cameraOff, setCameraOff] = useState(false);
+  let videoRecorder = null;
+
+  const toggleCameraState = () => {
+    if (!cameraOff) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+    setCameraOff(!cameraOff);
+  };
+
+  const onVideoDataAvailable = (data) => {};
+
+  const onStart = () => {};
+
+  const stopRecording = () => {
+    videoRecorder.stop();
+  };
+
+  const startRecording = (time = 3000) => {
+    videoRecorder.start();
+  };
 
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
-        .getUserMedia({ video: true })
+        .getUserMedia({ video: true, audio: true })
         .then((stream) => {
-          videoRef.current.srcObject = stream;
+          videoRecorder = new MediaRecorder(stream);
+          videoRecorder.ondataavailable = onVideoDataAvailable;
+          videoRecorder.onstart = onStart;
+          videoRef.current.srcObject = videoRecorder.stream;
           videoRef.current.play();
         })
         .catch((err) => {});
     }
   }, []);
-  return <video ref={videoRef}></video>;
+  return (
+    <div>
+      <button onClick={startRecording}>Start</button>
+      <button onClick={stopRecording}>Stop</button>
+      <button onClick={toggleCameraState}>Off</button>
+      <video ref={videoRef}></video>
+    </div>
+  );
 };
 
 export default VideoComponent;
